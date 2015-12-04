6576d84e1da163f75f8a6de89fcb850815c3de6e
Pierre Bourgeois <dragonralph@gmail.com>
2015-12-04 04:19:15 +0100

    Front

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,20 @@
 /**
  * Created by pierre on 04/12/15.
  */
-$(function(){
-    $
+$(function () {
+    $("form").submit(function (e) {
+        e.preventDefault();
+        var val = $(this).find('input').val();
+        var id = $(this).find('input').attr('data-id');
+        $(this).find('input').attr('disabled', 'disabled');
+        $.post($(this).attr('action'), {'id': id, 'val': val}, function (data) {
+            if (data != "fail") {
+                alert(data);
+                location.reload();
+            } else {
+                alert("Oups... Une erreur est survenue...");
+            }
+        });
+
+    })
 });
---
+++ controllers/Dons.php
@@ -0,0 +1,27 @@
+<?php
+
+class Dons extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function homeAction()
+    {
+
+        if($_SERVER['REQUEST_METHOD'] != "POST"){
+            exit("fail");
+        }
+        $val = (double)$_POST['val'];
+        $id = (int)$_POST['id'];
+
+
+        if($val > 0){
+            echo "Merci ! Ce don de " . $val . " euros permettra d'acheter ce matériel de première nécessité !";
+            exit;
+        }
+        echo "fail";
+    }
+}
