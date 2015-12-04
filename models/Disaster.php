01c5455ee6601c0b996cce9b82f71b0b031cf4be
Quentin Brault <tititesouris@laposte.net>
2015-12-04 02:08:08 +0100

    debut disastre

--- models/Disaster.php
+++ models/Disaster.php
@@ -1,14 +1,23 @@
 <?php
 class Disaster extends Model {
+
     private $name;
+
+    private $descritpion;
     
     private $date;
-    private  $id;
+
+    private $id;
+
     private $ref;
+
     private $incident;
+
     private $product;
-    private  $image;
-            function __construct($name,$id,$date,$ref,$incident,$product,$image,$total_amout,$total_product){ {
+
+    private $image;
+
+    function __construct($name, $id, $date, $ref, $incident, $product, $image, $total_amount, $total_product) {
         parent::__construct();
         $this->name = $name;
         $this->id = $id;
@@ -17,9 +26,8 @@ class Disaster extends Model {
         $this->incident = $incident;
         $this->product = $product;
         $this->image = $image;
-        $this->total_amout = $total_amout;
+        $this->total_amout = $total_amount;
         $this->total_product = $total_product;
-        
     }
 
 }
