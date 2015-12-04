42dc25ec87218913fdfee194f185a97d925df560
Ludo <ludovic.ouvry@aliceadsl.fr>
2015-12-04 02:02:38 +0100

    disater model

--- models/Disaster.php
+++ models/Disaster.php
@@ -1,4 +1,25 @@
 <?php
 class Disaster extends Model {
+    private $name;
+    
+    private $date;
+    private  $id;
+    private $ref;
+    private $incident;
+    private $product;
+    private  $image;
+            function __construct($name,$id,$date,$ref,$incident,$product,$image,$total_amout,$total_product){ {
+        parent::__construct();
+        $this->name = $name;
+        $this->id = $id;
+        $this->date = $date;
+        $this->ref = $ref;
+        $this->incident = $incident;
+        $this->product = $product;
+        $this->image = $image;
+        $this->total_amout = $total_amout;
+        $this->total_product = $total_product;
+        
+    }
 
 }
