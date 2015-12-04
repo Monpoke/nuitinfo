f5bd5f55606ef0ed400a57d467678847792000d5
Pierre Bourgeois <dragonralph@gmail.com>
2015-12-04 02:22:32 +0100

    Inté

--- controllers/Index.php
+++ controllers/Index.php
@@ -11,11 +11,17 @@ class Index extends Controller
     public function homeAction()
     {
 
-        $this->loadModel("Disaster");
+        $this->loadModel("Disaster",true);
 
         $disasters = array(
-            new Disaster("Tsunami bancock", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true)),
-            new Disaster("Earthquake LA", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true))
+//            new Disaster("Tsunami bancock", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true)),
+//            new Disaster("Earthquake LA", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true))
+
+            new Disaster("Tsuna", 10, "20 janvier 2015", "POL", "TRUC", "Bouteille","eau",100, 50),
+            new Disaster("Tsuna", 10, "20 janvier 2015", "POL", "TRUC", "Bouteille","eau",100, 50),
+            new Disaster("Tsuna", 10, "20 janvier 2015", "POL", "TRUC", "Bouteille","eau",100, 50),
+            new Disaster("fdfdsfsdf", 10, "89 décembre 2015", "Magie", "Pliefef", "Bouteille","eau",100, 0),
+
         );
         $this->view("index", array(
             'disasters' => $disasters
