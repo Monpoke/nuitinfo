f6f2a7434cb42030961acc340cdcf599594f746a
Pierre Bourgeois <dragonralph@gmail.com>
2015-12-04 05:45:38 +0100

    Add desastre

--- controllers/Index.php
+++ controllers/Index.php
@@ -11,12 +11,19 @@ class Index extends Controller
     public function homeAction()
     {
 
+        /** @var Articles $Dons */
+        $Dons=$this->loadModel("Articles");
+        $articles = $Dons->getArticlesByPriority();
+
         $this->loadModel("Disaster", true);
+
+        // Récupération des prix
+
 
         $p = new Product(1, "Eau", 100, "Boisson", "Basique...", true);
 
         $d = "";
-        for($i=0;$i<20;$i++){
+        for ($i = 0; $i < 20; $i++) {
             $d .= "Un texte...Un texte...Un texte...\n";
         }
 
