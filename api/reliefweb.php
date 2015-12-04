43aab39960f44fcf5ce7945d6832ed40fe494f71
Ludo <ludovic.ouvry@aliceadsl.fr>
2015-12-04 04:54:17 +0100

    getDisaters in reliefweb

--- api/reliefweb.php
+++ api/reliefweb.php
@@ -14,69 +14,61 @@ echo trouverRef($tab, $num);
 echo trouverNom($tab, $num);
 echo trouverIncident($tab, $num);
 
-function trouverDate($tab, $num) { // permmetra un classement par date 
-    $nom = $tab["data"][$num]["fields"]["name"];
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    // echo $nom;
-    echo "<br>";
-    echo "<br>";
-    $nomtab = explode(" - ", $nom);
-    return $nomtab[1];
-}
+  function trouverDate($tab, $num) { // permmetra un classement par date 
+        $nom = $tab["data"][$num]["fields"]["name"];
 
-function trouverId($tab, $num) { // permmetra un classement par date 
-    $id = $tab["data"][$num]["id"];
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    // echo $id;
-    echo "<br>";
-    echo "<br>";
 
-    return $id;
-}
+        $nomtab = explode(" - ", $nom);
+        return $nomtab[1];
+    }
 
-function trouverRef($tab, $num) { // permmetra un classement par date 
-    $href = $tab["data"][$num]["href"];
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    // echo $id;
-    echo "<br>";
-    echo "<br>";
+    function trouverId($tab, $num) { // permmetra un classement par date 
+        $id = $tab["data"][$num]["id"];
 
-    return $href;
-}
 
-function trouverNom($tab, $num) { // permmetra un classement par date 
-    $nom = $tab["data"][$num]["fields"]["name"];
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    $nomtab = explode(" - ", $nom);
-    $nomtab2 = explode(": ", $nomtab[0]);
-    echo "<br>";
-    echo "<br>";
+        return $id;
+    }
 
-    return $nomtab2[0];
-}
+    function trouverRef($tab, $num) { // permmetra un classement par date 
+        $href = $tab["data"][$num]["href"];
 
-function trouverIncident($tab, $num) { // permmetra un classement par date 
-    $nom = $tab["data"][$num]["fields"]["name"];
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    $nomtab = explode(" - ", $nom);
-    $nomtab2 = explode(": ", $nomtab[0]);
-    echo "<br>";
-    echo "<br>";
 
-    return $nomtab2[1];
-}
+        return $href;
+    }
+
+    function trouverNom($tab, $num) { // permmetra un classement par date 
+        $nom = $tab["data"][$num]["fields"]["name"];
+
+        if (strpos($nom, ":") === false) {
+            $nomtab = explode(" - ", $nom);
+        } else {
+            $nomtab = explode(": ", $nomtab[0]);
+        }
+
+        return $nomtab[0];
+    }
+
+    function trouverIncident($tab, $num) { // permmetra un classement par date 
+        $nom = $tab["data"][$num]["fields"]["name"];
+
+        $nomtab = explode(" - ", $nom);
+        $nomtab2 = explode(": ", $nomtab[0]);
+        echo "<br>";
+        if (isset($nomtab2[1])) {
+            return $nomtab2[1];
+        } else {
+            $tab=trouverRef($tab, $num);
+            
+        }
+    }
+    function trouverDescription($tab, $num){
+       $file=trouverRef($tab, $num);
+       $tab2 = json_decode($file, true);
+       return $tab2["data"]["fields"]["description"];
+    }
+    function get_disater($tab, $num){
+        return new Disaster(trouverId($tab, $num), trouverNom($tab, $num), trouverIncident($tab, $num), trouverDate($tab, $num), trouverDescription($tab, $num), null, null, null, null);
+            
+    
+    }
+
