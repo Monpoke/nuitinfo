5edffaf86d17c9aaebf86a5a630e02409d50311f
Quentin Brault <tititesouris@laposte.net>
2015-12-04 01:58:39 +0100

    dummy disasters

--- controllers/Index.php
+++ controllers/Index.php
@@ -11,7 +11,10 @@ class Index extends Controller
     public function homeAction()
     {
 
-
+        $disasters = array(
+            new Disaster("Tsunami bancock", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true)),
+            new Disaster("Earthquake LA", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true))
+        );
         $this->view("index");
     }
 
