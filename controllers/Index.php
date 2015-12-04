3db9f37aff4402fc95136c04d784402c5745bc80
Pierre Bourgeois <dragonralph@gmail.com>
2015-12-04 03:35:26 +0100

    Front

--- controllers/Index.php
+++ controllers/Index.php
@@ -13,7 +13,7 @@ class Index extends Controller
 
         $this->loadModel("Disaster", true);
 
-        $p = new Product("Eau", 1.5, "Boisson", "Basique...", true);
+        $p = new Product("Eau", 100, "Boisson", "Basique...", true);
 
         $d = "";
         for($i=0;$i<20;$i++){
@@ -24,9 +24,9 @@ class Index extends Controller
 //            new Disaster("Tsunami bancock", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true)),
 //            new Disaster("Earthquake LA", new Product("water", 2.5, new ProductCategory("test", "dasd"), "dasd", true))
 
-            new Disaster(1, "France", "Tsunami", "20 janvier", $d, "eau.png", $p, 20, 100),
-            new Disaster(2, "France", "Tsunami", "20 janvier", $d, "eau.png", $p, 80, 200),
-            new Disaster(3, "France", "Tsunami", "20 janvier", $d, "eau.png", $p, 10, 100),
+            new Disaster(1, "France", "Tsunami", "20 janvier", $d, "eau.png", $p, 1200, 20),
+            new Disaster(2, "France", "Tsunami", "20 janvier", $d, "eau.png", $p, 1200, rand(0,100)),
+            new Disaster(3, "France", "Tsunami", "20 janvier", $d, "eau.png", $p, 1200, rand(0,100)),
 
         );
         $this->view("index", array(
